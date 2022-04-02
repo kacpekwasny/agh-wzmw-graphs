0ff0f7f9a1f7285f1a0d4db992c4093da59bdeb8
Kacper Kwasny <kwasny.it@gmail.com>
2022-04-02 13:44:17 +0200

    before remove join_node_to_edge funcs from Node

--- libs/edges/edge.py
+++ libs/edges/edge.py
@@ -1,12 +1,32 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from ..errors.errors import NodeNotMemberOfEdgeError
+
 if TYPE_CHECKING:
     from ..nodes.node import Node
 
 class Edge:
     def __init__(self, *nodes: Node) -> None:
-        self.nodes = nodes
+        self.nodes = list(nodes)
         
+    def connect_node(self, n_new: Node):
+        for n in self.nodes:
+            if n_new in n.neighbours:
+                n.neighbours[n] += 1
+                continue
+            n.neighbours[n_new] = 1
+        self.nodes.append(n_new)
 
+    def disconnect_node(self, n_old: Node):
+        """
+        remove first occurance of node in edge    
+        """
+        if n_old not in self.nodes:
+            raise NodeNotMemberOfEdgeError
+        self.nodes.remove(n_old)
+        for n in self.nodes:
+            n.neighbours[n_old] -= 1
+            n_old[n] -= 1
+        
 
--- libs/nodes/node.py
+++ libs/nodes/node.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from itertools import count
 from typing import Type
 
 from ..edges.edge import Edge
@@ -6,101 +7,109 @@ from ..graphs.graph_base import GraphBase
 from ..errors.errors import EdgeNotFoundNodeError, NodeMissingIdNodeError, NodeMissingIdNodeError, NodeNotFoundNodeError, NodeNotMemberOfEdgeError, WrongGraphParentNodeError
 
 class Node:
-    def __init__(self, parent_graph: GraphBase, id_=None) -> None:
-        self.parent_graph: GraphBase = parent_graph
-        self.neighbours: list[Node] = []
-        self.edges: list[Edge] = []
+    def __init__(self, id_=None) -> None:
+        """
+            params:
+                parent_graph: GraphBase - graph that this node is a part of
+                id_: int - To allow easier interactions for user 
+        """
         if id_ is None:
             raise NodeMissingIdNodeError
+        self.neighbours: dict[Node, int] = {} # Node -> times it is connected to self.
+        self.edges: list[Edge] = []
         self.id = id_
     
-    """
-    add_neighbours adds nodes to neighbours if this node allready is not present in neighbours
-    """
+
     def __add_neighbours(self, *nodes: Node):
+        """
+        add_neighbours adds nodes to neighbours and increments the neighbours count for that node
+        If an error was raised no changes were commited to the node.
+            raises:
+                WrongGraphParentNodeError: NodeError - if the node to become neighbour has a different graph as a parent
+        """
         for n in nodes:
-            if n.parent_graph is not self.parent_graph:
-                raise WrongGraphParentNodeError
-            self.neighbours.append(n)
+            if n not in self.neighbours:
+                self.neighbours[n] = 1
+                continue
+            self.neighbours += 1    
 
-    """
-    removes nodes from self.neighbours
-        params:
-            nodes: list[Node] - remove nodes
-        raises:
-            NodeNotFoundNodeError: NodeError - one of nodes to be removed from
-    """
     def __remove_neighbour(self, *nodes: Node):
-        for n in nodes:
-            try:
-                self.neighbours.remove(n)
-            except ValueError:
-                raise NodeNotFoundNodeError
+        """
+        removes decrements self.neighbours[node].
 
-    """
-    append edge to edges of this node if this node is a part of this edge
-        raises:
-            NodeNotMemberOfEdgeError: EdgeError
-    """
+            params:
+                nodes: tuple[Node] - remove nodes
+            raises:
+                NodeNotFoundNodeError: NodeError - one of nodes to be removed from
+        """
+        for n in nodes:
+            if n in self.neighbours:
+                self.neighbours[n] -= 1
+            
+
+
     def __add_edge(self, e: Edge):
+        """
+        append edge to edges of this node if this node is a part of this edge
+            raises:
+                NodeNotMemberOfEdgeError: EdgeError
+        """
         if self not in e.nodes:
             raise NodeNotMemberOfEdgeError
         self.edges.append(e)
 
-    """
-    remove edge, and remove edge.nodes from neighbours of this node !!!! but only if other edge doesnt connect this node to that other
 
-        params:
-            e: Edge
-        
-        raises:
-            NodeNotMemberOfEdgeError:   EdgeError - this node is not member of edge.nodes
-            EdgeNotFoundNodeError:      NodeError - this edge is not member of node.edges
-    """
     def __remove_edge(self, e: Edge):
-        if self not in e.nodes:
-            raise NodeNotMemberOfEdgeError
-        if e not in self.edges:
-            raise EdgeNotFoundNodeError
+        """
+        remove edge, and decrese count on times self and nodes from edge.nodes are neighbours in self.neighbours
+        decrese count self is neighbour in others
+
+            params:
+                e: Edge
+            
+            raises:
+                NodeNotMemberOfEdgeError:   EdgeError - this node is not member of edge.nodes
+                EdgeNotFoundNodeError:      NodeError - this edge is not member of node.edges
+        """
         self.edges.remove(e)
         for n in e.nodes:
-            if n is self:
-                continue
             # find if there is another edge that makes self neighbours with n
             # if so then continue
             # if not then remove n from self.neighbours
-            found_node_in_other_edge = False
-            for e in self.edges:
-                if n in e.nodes:
-                    found_node_in_other_edge = True
-                    break
-            if not found_node_in_other_edge:
-                self.
-                
+            self.neighbours[n] -= 1
 
-
-    """
-    Join this node to edge. Append edge to this node's edges if this node is a part of this edge's nodes.
-        params:
-            e: Edge - to be connected to
-        
-        raises:
-            NodeNotMemberOfEdgeError: EdgeError - trying to connect a node to an edge that it is not a member of
-    """
     def join_node_to_edge(self, e: Edge):
-        if self not in e.nodes:
-            raise NodeNotMemberOfEdgeError
-        self.__add_neighbours([n for n in e.nodes if n is not self])
+        """
+        Join this node to edge.
+        Append edge to this node's edges if this node is a part of this edge's nodes.
+
+            params:
+                e: Edge - to be connected to
+            
+        """
+        e.connect_node(self)
+        return
+        e.nodes.append(self)
+        self.__add_neighbours(e.nodes)
         self.__add_edge(e)
 
 
-    """
-    Remove edge from edges, remove nodes from neighbours !but only if they
-    """
     def disconnect_node_from_edge(self, e: Edge):
+        """
+        Remove edge from edges, decreses the count nodes are neighbours
+            raises:
+                EdgeNotFoundNodeError: NodeError - this edge was not found in node's edges
+        """
+        e.disconnect_node(self)
+        return 
         if e not in self.edges:
             raise EdgeNotFoundNodeError
+        self.__remove_edge(e)
         
+
+    @property
+    def num_neighbours(self) -> int:
+        return len([None for v in self.neighbours.values() if v != 0])
 
 
         
