686085abcfe6c5aa9968db6eecf3c873d9447838
Kacper Kwasny <kwasny.it@gmail.com>
2022-04-08 22:06:51 +0200

    better errors

--- graphlib/errors/errors.py
+++ graphlib/errors/errors.py
@@ -1,17 +1,22 @@
 
+class GraphError(Exception):pass
+NodeNotMemberOfGraphError = GraphError("Node is not a member of this graph / id was not found")
+EdgeNotMemberOfGraphError = GraphError("Edge is not a member of this graph / id was not found")
+
+EdgeAllreadyExistsinGraphError = GraphError("Edge allready exists in graph")
+NodesNotConnectedGraphError = GraphError("Nodes are not connected")
+NodesAllreadyConnectedGraphError = GraphError("Those two nodes are allready connected")
+
+CannotCreateLoopGraphError = GraphError("It is not allowed to create an edge from N1 to N1")
+NotMultigraphGraphError = GraphError("This graph is not a multigraph, and thus doesn't allow this type of edge")
+NoNodesGraphError = GraphError("This graph has no nodes")
+
+
 class NodeError(Exception):pass
-NodeNotFoundNodeError = NodeError(f"At least one Node was not found in graph's vertices.")
 NodeMissingIdNodeError = NodeError("No id was passed")
 WrongGraphParentNodeError = NodeError("Cannot add a node with different graph parent as neighbour")
 EdgeNotFoundNodeError = NodeError("Edge has not been found in edges of this node")
 
-class GraphError(Exception):pass
-EdgeAllreadyExistsinGraphError = GraphError("Edge allready exists in graph.")
-NodesAllreadyConnectedGraphError = GraphError("Those two nodes are allready connected")
-EdgeNotMemberOfGraphError = GraphError("Edge not a member of this graph")
-NodesNotConnectedGraphError = GraphError("Nodes are not connected")
-CannotCreateLoopGraphError = GraphError("It is not allowed to create an edge from N1 to N1.")
-NoNodesGraphError = GraphError("This graph has no nodes.")
 
 class EdgeError(Exception):pass
 NodeNotMemberOfEdgeError = EdgeError("Node is not a member of edge")
